8170f9e011e6e222c92514947a04df27105950d4
Sujoy Ray <2013439@deviceind.autoever.com>
2026-03-20 15:00:44 +0530

    url fix

--- src/api.ts
+++ src/api.ts
@@ -35,6 +35,15 @@ export const fetchLocations = async (
   filters?: Partial<{ name: string; type: string; dimension: string }>
 ): Promise<LocationResponse> => {
   const params = { page, ...filters };
+
+  const searchParams = new URLSearchParams();
+  Object.entries(params).forEach(([key, value]) => {
+    if (value) searchParams.set(key, String(value));
+  });
+  
+  const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+  window.history.pushState({}, "", newUrl);
+
   const res = await axios.get<LocationResponse>(`${API_URL}/location`, { params });
   return res.data;
 };
@@ -50,11 +59,29 @@ export const fetchEpisodes = async (
   filters?: Partial<{ name: string; episode: string }>
 ): Promise<EpisodeResponse> => {
   const params = { page, ...filters };
+
+  const searchParams = new URLSearchParams();
+  Object.entries(params).forEach(([key, value]) => {
+    if (value) searchParams.set(key, String(value));
+  });
+  
+  const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+  window.history.pushState({}, "", newUrl);
+
   const res = await axios.get<EpisodeResponse>(`${API_URL}/episode`, { params });
   return res.data;
 };
 
 export const fetchEpisodeById = async (id: number | number[]): Promise<Episode | Episode[]> => {
+
+  const searchParams = new URLSearchParams();
+  Object.entries({ id }).forEach(([key, value]) => {
+    if (value) searchParams.set(key, String(value));
+  });
+  
+  const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+  window.history.pushState({}, "", newUrl);
+
   const res = await axios.get<Episode | Episode[]>(`${API_URL}/episode/${id}`);
   return res.data;
 };
